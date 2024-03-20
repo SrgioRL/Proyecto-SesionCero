401ed8f6fb1b9a8cf14b596cda922839c2b97612
SergioRL <sergiorl1989@gmail.com>
2024-03-20 17:58:10 +0100

    First commit. Back and Front projects created

---
+++ SesionCero_Backend/src/main/java/sesioncero/SesionCeroBackendApplication.java
@@ -0,0 +1,13 @@
+package sesioncero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SesionCeroBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SesionCeroBackendApplication.class, args);
+	}
+
+}
---
+++ SesionCero_Backend/src/test/java/sesioncero/SesionCeroBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package sesioncero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SesionCeroBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
