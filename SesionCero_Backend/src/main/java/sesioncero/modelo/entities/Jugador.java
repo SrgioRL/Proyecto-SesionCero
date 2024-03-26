1edbe139b25bcf00881fd6affe88523f25060ed7
bea.deval <beadeval22@gmail.com>
2024-03-26 17:18:21 +0100

    Añadir entidades

--- SesionCero_Backend/src/main/java/sesioncero/modelo/entities/Jugador.java
+++ SesionCero_Backend/src/main/java/sesioncero/modelo/entities/Jugador.java
@@ -1,5 +1,31 @@
 package sesioncero.modelo.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity // Indica que esta clase es una entidad JPA
+@Table(name="Jugadores") // Especifica el nombre de la tabla en la base de datos
+@Data // Anotación de Lombok para generar automáticamente getters, setters, toString, etc.
+@NoArgsConstructor // Constructor sin argumentos generado automáticamente por Lombok
+@AllArgsConstructor
+
 public class Jugador {
 
+
+    @Id // Indica que este campo es la clave primaria de la entidad
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Generación automática de valores para la clave primaria
+    @Column(name="id_jugador") // Especifica el nombre de la columna en la base de datos
+    private int idJugador; // Identificador único del jugador
+    private String nombre;
+    private String apellido1;
+    private String apellido2;
+    private String email;
+    private String contraseña;
 }
